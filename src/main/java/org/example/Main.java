39ac5249d845d135bae47bde8c26bdf2ce145200
MarwanMohammed <marwan.m.rabea@gmail.com>
2023-11-30 10:36:06 +0200

    Add Main Class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,11 @@ package org.example;
 
 import WebPages.LogInPage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Main {
-    public static void main(String[] args) {
-
-    }
+    public static void main(String[] args){
+  }
 }
